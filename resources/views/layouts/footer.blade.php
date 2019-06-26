471d5ed3910332ae27a35d4f4de96e4765f0cd8b
Steve <steve.pronk@pos4.nl>
2019-06-26 16:42:25 +0200

    Added the setup

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="container">
+    <div class="bg-dark w-100">
+        <div class="container text-white text-center">
+            Made with love by <a href="https://stpronk.nl/" class="text-white">StPronk</a>
+        </div>
+    </div>
+</footer>
