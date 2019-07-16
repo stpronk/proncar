83c03ac0f1fd036df80018bfb199b078edf1a4a2
stpronk <stpronk@gmail.com>
2019-07-16 19:28:20 +0200

    Deleted commented code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,16 +2,6 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-{{--                <ul class="list-inline mb-2">--}}
-{{--                    <li class="list-inline-item text-white">--}}
-{{--                        <a href="#">About</a>--}}
-{{--                    </li>--}}
-{{--                    <li class="list-inline-item text-white">&sdot;</li>--}}
-{{--                    <li class="list-inline-item text-white">--}}
-{{--                        <a href="#">Contact</a>--}}
-{{--                    </li>--}}
-{{--                    <li class="list-inline-item text-white">&sdot;</li>--}}
-{{--                </ul>--}}
                 <p class="small mb-4 mb-lg-0">&copy; Proncar ~ 2017. <br>
                     Made with love by <a class="text-white" href="https://stpronk.nl/">StPronk</a></p>
             </div>
