b71fea5dfb8e84b4b3e2d389b16be9572f38ba7b
Steve <steve.pronk@pos4.nl>
2019-07-16 17:34:46 +0200

    Removed dupe nav

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,53 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Navigation -->
-    @include('layouts.navbar')
 
     <!-- Masthead -->
     @include('layouts.header')
-        <section class="call-to-action bg-white">
-            <div class="container">
-                <div class="row no-gutters">
 
-                    <div class="col-12 row">
-                        <h2 class="col-12 mb-4 text-center text-primary">Contact us!</h2>
+    <section class="call-to-action bg-white">
+        <div class="container">
+            <div class="row no-gutters">
+
+                <div class="col-12 row">
+                    <h2 class="col-12 mb-4 text-center text-primary">Contact us!</h2>
+                </div>
+
+                <form class="form-group col-12 font-weight-bold px-4 mb-6">
+                    <div class="form-group row">
+                        <label class="col-form-label col-12" for="email" type="email">E-mail Address</label>
+                        <input class="form-control col-12" name="email" id="email" type="email" value="" placeholder="">
                     </div>
 
-                    <form class="form-group col-12 font-weight-bold px-4 mb-6">
-                        <div class="form-group row">
-                            <label class="col-form-label col-12" for="email" type="email">E-mail Address</label>
-                            <input class="form-control col-12" name="email" id="email" type="email" value="" placeholder="">
-                        </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-12" for="phone" type="text">Phone number</label>
+                        <input class="form-control col-12" name="phone" id="phone" type="text" value="" placeholder="">
+                    </div>
 
-                        <div class="form-group row">
-                            <label class="col-form-label col-12" for="phone" type="text">Phone number</label>
-                            <input class="form-control col-12" name="phone" id="phone" type="text" value="" placeholder="">
-                        </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-12" for="name" type="text">Name</label>
+                        <input class="form-control col-12" name="name" id="name" type="text" value="" placeholder="">
+                    </div>
 
-                        <div class="form-group row">
-                            <label class="col-form-label col-12" for="name" type="text">Name</label>
-                            <input class="form-control col-12" name="name" id="name" type="text" value="" placeholder="">
-                        </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-12" for="subject" type="text">Subject</label>
+                        <input class="form-control col-12" name="subject" id="subject" type="text" value="" placeholder="">
+                    </div>
 
-                        <div class="form-group row">
-                            <label class="col-form-label col-12" for="subject" type="text">Subject</label>
-                            <input class="form-control col-12" name="subject" id="subject" type="text" value="" placeholder="">
-                        </div>
+                    <div class="form-group row">
+                        <label class="col-form-label col-12" for="message">Message</label>
+                        <textarea class="form-control col-12" name="message" id="subject" type="text" rows="8"></textarea>
+                    </div>
 
-                        <div class="form-group row">
-                            <label class="col-form-label col-12" for="message">Message</label>
-                            <textarea class="form-control col-12" name="message" id="subject" type="text" rows="8"></textarea>
-                        </div>
+                    <div class="form-group row">
+                        <button class="btn btn-block btn-primary btn-lg col-12" type="submit">Send</button>
+                    </div>
+                </form>
 
-                        <div class="form-group row">
-                            <button class="btn btn-block btn-primary btn-lg col-12" type="submit">Send</button>
-                        </div>
-                    </form>
-
-                </div>
             </div>
-        </section>
+        </div>
+    </section>
 
     <section class="features-icons text-center bg-light">
         <div class="container">
@@ -58,14 +57,16 @@
                         <div class="row">
                             <div class="col-6">
                                 <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                                    <a class="text-decoration-none features-icons-icon d-flex" href="https://www.facebook.com/Proncar-468839900135515/" target="_blank">
+                                    <a class="text-decoration-none features-icons-icon d-flex" href="https://www.facebook.com/Proncar-468839900135515/"
+                                       target="_blank">
                                         <i class="icon-social-facebook m-auto text-primary"></i>
                                     </a>
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                                    <a class="text-decoration-none features-icons-icon d-flex" href="https://www.instagram.com/proncar_zoetermeer/" target="_blank">
+                                    <a class="text-decoration-none features-icons-icon d-flex" href="https://www.instagram.com/proncar_zoetermeer/"
+                                       target="_blank">
                                         <i class="icon-social-instagram m-auto text-primary"></i>
                                     </a>
                                 </div>
@@ -77,7 +78,7 @@
         </div>
     </section>
 
-    <section class="company-info">
-        <!-- todo -->
-    </section>
+<section class="company-info">
+    <!-- todo -->
+</section>
 @endsection
