6494bdf0d8cd19083dabd2d8b77021d2d9074df0
stpronk <stpronk@gmail.com>
2019-07-17 23:02:07 +0200

    Changed mobile responsiveness

--- resources/views/sections/feature-icons.blade.php
+++ resources/views/sections/feature-icons.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row">
             @foreach($content['items'] as $item)
-                <div class="col-lg-{{ 12 / $content['item_count'] }}">
-                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 text-center">
+                <div class="col-md-{{ 12 / $content['item_count'] }}">
+                    <div class="features-icons-item mx-auto mb-5 mb-md-0 mb-md-3 text-center">
                         <div class="features-icons-icon d-flex">
                             <i class="icon-{{ $item['icon'] }} m-auto text-primary"></i>
                         </div>
