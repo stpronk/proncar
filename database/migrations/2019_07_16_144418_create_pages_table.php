ccfeb7848b45812516e5fee04285ba68b1392970
Steve Pronk <steve@competa.com>
2019-11-06 13:17:55 +0100

    Filled migration for pages

--- database/migrations/2019_07_16_144418_create_pages_table.php
+++ database/migrations/2019_07_16_144418_create_pages_table.php
@@ -15,6 +15,22 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->string('selector', 100);
+            $table->string('name', 100);
+            $table->string('title', 100);
+            $table->string('route_url');
+            $table->boolean('route_auth')->default(false);
+            $table->string('route_controller');
+            $table->boolean('nav_hidden')->default(true);
+            $table->string('nav_name');
+            $table->string('nav_class');
+            $table->integer('template_id')->default(1);
+            $table->string('template_blade')->default('default');
+            $table->string('type')->default('get');
+            $table->uuid('uuid');
+            $table->boolean('hidden')->default(true);
+
             $table->timestamps();
         });
     }
