8a496e09e4cf69b23ba65a627dc07a32d1ab9579
Steve Pronk <steve@competa.com>
2020-01-13 20:01:05 +0100

    Removed comments

--- app/Helpers.php
+++ app/Helpers.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Auth;
 
 if(!function_exists('page_index'))
 {
-//    function page_index($index = null){
-//        if($index){
-//            return \Spatie\Valuestore\Valuestore::make(storage_path('content/pages_index.json'))->all()['pages'][$index];
-//        }
-//        return \Spatie\Valuestore\Valuestore::make(storage_path('concept/pages_index.json'))->all()['pages'];
-//    }
+    function page_index($index = null){
+        if($index){
+            return \Spatie\Valuestore\Valuestore::make(storage_path('content/pages_index.json'))->all()['pages'][$index];
+        }
+        return \Spatie\Valuestore\Valuestore::make(storage_path('concept/pages_index.json'))->all()['pages'];
+    }
 }
 
 if(!function_exists('page_sections'))
 {
-//    function page_sections($index){
-//        return \Spatie\Valuestore\Valuestore::make(storage_path('content/pages/'.$index.'.json'))->all();
-//    }
+    function page_sections($index){
+        return \Spatie\Valuestore\Valuestore::make(storage_path('content/pages/'.$index.'.json'))->all();
+    }
 }
